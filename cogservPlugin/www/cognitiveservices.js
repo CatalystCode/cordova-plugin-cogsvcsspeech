66ae70c12f4592fdd565119ab8e6ca7af64096be
Elizabeth Graham <esenoch@hotmail.com>
2020-01-28 10:03:21 -0500

    Added plugin and native ionic code (#18)
    * Added plugin and native ionic code
    
    * Removed the cognitive services plugin .DS_Store file from gitignore, and added all .DS_Store files.

---
+++ cogservPlugin/www/cognitiveservices.js
@@ -0,0 +1,37 @@
+
+var exec = require('cordova/exec');
+
+module.exports  = {
+    SetSubscription: function(speechSubscriptionKey, serviceRegion, successCallback, errorCallback) {
+        return cordova.exec(successCallback, errorCallback, 'CognitiveServices', 'SetSubscription', [speechSubscriptionKey, serviceRegion]);
+    },
+
+    SpeakText: function(speechText, successCallback, errorCallback) {
+        return cordova.exec(successCallback, errorCallback, 'CognitiveServices', 'SpeakText', [speechText]);
+    },
+
+   SpeakTextAsync: function(speechText, successCallback, errorCallback) {
+        return cordova.exec(successCallback, errorCallback, 'CognitiveServices', 'SpeakTextAsync', [speechText]);
+    },
+
+    SpeakSsml: function(speechText, successCallback, errorCallback) {
+        return cordova.exec(successCallback, errorCallback, 'CognitiveServices', 'SpeakSsml', [speechText]);
+    },
+
+    SpeakSsmlAsync: function(speechText, successCallback, errorCallback) {
+        return cordova.exec(successCallback, errorCallback, 'CognitiveServices', 'SpeakSsmlAsync', [speechText]);
+    },
+
+    RecognizeFromMicrophone: function(successCallback, errorCallback) {
+        return cordova.exec(successCallback, errorCallback, 'CognitiveServices', 'RecognizeFromMicrophone' );
+    },
+
+    StopListening: function(successCallback, errorCallback) {
+        return cordova.exec(successCallback, errorCallback, 'CognitiveServices', 'StopListening' );
+    },
+
+    SpeakStop: function(successCallback, errorCallback) {
+        return cordova.exec(successCallback, errorCallback, 'CognitiveServices', 'SpeakStop' );
+    }
+
+}
